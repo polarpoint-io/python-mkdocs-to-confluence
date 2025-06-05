962f4a64f86e6b0a6685bca5b8e513e3cc23e4b2
Surjit Bains <surjit.bains@newday.co.uk>
2025-06-05 16:46:20 +0100

    feat: initial commit

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,21 +1,16 @@
 import pytest
-from plugin import ConfluenceUploaderPlugin
-from mkdocs.config.defaults import get_schema, load_config
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from mkdocs_confluence_plugin.plugin import MkdocsToConfluence
+from mkdocs.config import load_config
 from mkdocs.structure.pages import Page
 from mkdocs.structure.files import Files
 
+
+
 def test_plugin_instantiation():
-    plugin = ConfluenceUploaderPlugin()
-    assert isinstance(plugin, ConfluenceUploaderPlugin)
-
-def test_on_post_build_runs(monkeypatch):
-    plugin = ConfluenceUploaderPlugin()
-
-    class DummyConfig:
-        site_dir = "site"
-        config_file_path = "mkdocs.yml"
-
-    try:
-        plugin.on_post_build(DummyConfig())
-    except Exception as e:
-        pytest.fail(f"Plugin failed on post_build: {e}")
+    plugin = MkdocsToConfluence()
+    assert isinstance(plugin, MkdocsToConfluence)
